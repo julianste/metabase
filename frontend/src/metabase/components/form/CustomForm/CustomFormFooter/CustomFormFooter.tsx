890941fe96c0a6c923faaf43687fef149c58d34b
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-13 12:31:59 +0100

    Use legacy Modal context for `CustomFormFooter`

--- frontend/src/metabase/components/form/CustomForm/CustomFormFooter/CustomFormFooter.tsx
+++ frontend/src/metabase/components/form/CustomForm/CustomFormFooter/CustomFormFooter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { t } from "ttag";
 
 import Button from "metabase/core/components/Button";
@@ -12,6 +13,7 @@ export interface CustomFormFooterProps {
   cancelTitle?: string;
   fullWidth?: boolean;
   isModal?: boolean;
+  isContextModal?: boolean;
   footerExtraButtons?: React.ReactElement[];
   onCancel?: () => void;
 }
@@ -23,9 +25,10 @@ function CustomFormFooter({
   footerExtraButtons,
   fullWidth,
   isModal,
+  isContextModal,
 }: CustomFormFooterProps) {
   return (
-    <CustomFormFooterStyled shouldReverse={isModal}>
+    <CustomFormFooterStyled shouldReverse={isModal || isContextModal}>
       <CustomFormSubmit fullWidth={fullWidth}>{submitTitle}</CustomFormSubmit>
       {onCancel && (
         <Button className="mx1" type="button" onClick={onCancel}>
@@ -37,5 +40,18 @@ function CustomFormFooter({
     </CustomFormFooterStyled>
   );
 }
+interface LegacyContextProps {
+  isModal?: boolean;
+}
 
-export default CustomFormFooter;
+// Modal components uses legacy React context to pass `isModal` prop
+const CustomFormFooterLegacyContext = (
+  props: CustomFormFooterProps,
+  { isModal }: LegacyContextProps,
+) => <CustomFormFooter {...props} isContextModal={isModal} />;
+
+CustomFormFooterLegacyContext.contextTypes = {
+  isModal: PropTypes.bool,
+};
+
+export default CustomFormFooterLegacyContext;
